ddd640971e29369d9e5543894c76b1abd71f4522
Aristotelia <jessi.moersdorf@gmail.com>
2021-09-12 22:31:13 +0200

    Maize contains the latest stage of the regression modelling code, stat_ut contains formulas for statistical evaluations

--- src/yield_regression_LoImodelling_soybean.py
+++ src/yield_regression_LoImodelling_soybean.py
@@ -11,16 +11,22 @@ if module_path not in sys.path:
 
 from src import params
 from src.plotter import Plotter
-from src import outdoor_growth
+from src import stat_ut
 from src.outdoor_growth import OutdoorGrowth
 import pandas as pd
 import geopandas as gpd
 import scipy
+from scipy import stats
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 params.importAll()
+
+
+'''
+Import data, extract zeros and explore data statistic values and plots 
+'''
 
 #import yield geopandas data for soybean
 
@@ -57,6 +63,162 @@ soyb_area_ha = sum(soyb_nozero['growArea'])
 print(soyb_area_ha)
 #65436615.57129341
 #102767896 FAOSTAT data for 2010
+
+
+
+'''
+Fitting of distributions to the data and comparing the fit
+'''
+
+pdf_list = []
+dist_list = []
+param_dict ={"Values":[]}
+x = np.linspace(0.01,
+                4500, 100)
+
+
+#Lognormal distribution
+dist_list.append('lognorm')
+#fit distribution to soyb yield data to get values for the parameters
+param1 = stats.lognorm.fit(soyb_kgha)
+#param_list.append(param1)
+param_dict["Values"].append(param1)
+print(param1)
+#use the parameters to calculate values for the probability density function 
+#(pdf) of the distribution
+pdf_fitted = stats.lognorm.pdf(x, *param1)
+#calculate the logarithmized pdf to calculate statistical values for the fit
+pdf_fitted_log = stats.lognorm.logpdf(soyb_kgha, *param1)
+pdf_list.append(pdf_fitted_log)
+#plot the histogram of the yield data and the curve of the lognorm pdf
+h = plt.hist(soyb_kgha, bins=50, density=True)
+plt.plot(x, pdf_fitted, lw=2, label="Fitted Lognormal distribution")
+plt.legend()
+plt.show()
+
+
+#Exponential distribution
+dist_list.append('exponential')
+#get parameters
+param2 = stats.expon.fit(soyb_kgha)
+param_dict["Values"].append(param2)
+print(param2)
+#calculate pdf
+pdf_fitted2 = stats.expon.pdf(x, *param2)
+#calculate log pdf
+pdf_fitted_log2 = stats.expon.logpdf(soyb_kgha, *param2)
+pdf_list.append(pdf_fitted_log2)
+#plot data histogram and pdf curve
+h = plt.hist(soyb_kgha, bins=50, density=True)
+plt.plot(x, pdf_fitted2, lw=2, label="Fitted Exponential distribution")
+plt.legend()
+plt.show()
+
+
+#Weibull distribution
+dist_list.append('weibull')
+#get parameters
+param3 = stats.weibull_min.fit(soyb_kgha)
+#param_list.append(param3)
+param_dict["Values"].append(param3)
+print(param3)
+#calculate pdf
+pdf_fitted3 = stats.weibull_min.pdf(x, *param3)
+#calculate log pdf
+pdf_fitted_log3 = stats.weibull_min.logpdf(soyb_kgha, *param3)
+pdf_list.append(pdf_fitted_log3)
+#plot data histogram and pdf curve
+h = plt.hist(soyb_kgha, bins=50, density=True)
+plt.plot(x, pdf_fitted3, lw=2, label="Fitted Weibull distribution")
+plt.legend()
+plt.show()
+
+
+#Gamma distribution
+dist_list.append('gamma')
+#get parameters
+param4 = stats.gamma.fit(soyb_kgha)
+#param_list.append(param4)
+param_dict["Values"].append(param4)
+print(param4)
+#calculate pdf
+pdf_fitted4 = stats.gamma.pdf(x, *param4)
+#calculate log pdf
+pdf_fitted_log4 = stats.gamma.logpdf(soyb_kgha, *param4)
+pdf_list.append(pdf_fitted_log4)
+#plot data histogram and pdf curve
+h = plt.hist(soyb_kgha, bins=50, density=True)
+plt.plot(x, pdf_fitted4, lw=2, label="Fitted Gamma distribution")
+plt.legend()
+plt.show()
+
+#Exponential Weibull distribution
+dist_list.append('exponential weibull')
+#get parameters
+param5 = stats.exponweib.fit(soyb_kgha)
+#param_list.append(param5)
+param_dict["Values"].append(param5)
+print(param5)
+#calculate pdf
+pdf_fitted5 = stats.exponweib.pdf(x, *param5)
+#calculate log pdf
+pdf_fitted_log5 = stats.exponweib.logpdf(soyb_kgha, *param5)
+pdf_list.append(pdf_fitted_log5)
+#plot data histogram and pdf curve
+h = plt.hist(soyb_kgha, bins=50, density=True)
+plt.plot(x, pdf_fitted5, lw=2, label="Fitted exponential weibull distribution")
+plt.legend()
+plt.show()
+
+#one in all plot
+h = plt.hist(soyb_kgha, bins=50, density=True)
+plt.plot(x, pdf_fitted, lw=2, label="Fitted Lognormal distribution")
+plt.plot(x, pdf_fitted2, lw=2, label="Fitted Exponential distribution")
+plt.plot(x, pdf_fitted3, lw=2, label="Fitted Weibull distribution")
+plt.plot(x, pdf_fitted4, lw=2, label="Fitted Gamma distribution")
+plt.plot(x, pdf_fitted5, lw=2, label="Fitted exponential weibull distribution")
+plt.legend()
+plt.show()
+
+
+#calculate loglik, AIC & BIC for each distribution
+stat_ut.stat_overview(dist_list, pdf_list, param_dict)
+#best fit so far: exponential weibull
+
+'''
+Load factor data and extract zeros
+'''
+#load explaining variables
+pesticides=pd.read_pickle(params.geopandasDataDir + 'SoybeanPesticidesByCrop.pkl')
+fertilizer=pd.read_pickle(params.geopandasDataDir + 'Fertilizer.pkl')
+irrigation=pd.read_pickle(params.geopandasDataDir + 'Irrigation.pkl')
+tillage=pd.read_pickle(params.geopandasDataDir + 'Tillage.pkl')
+
+#print the same row of each dataset to confirm that lats and lons align
+print(pesticides.loc[6040])
+print(fertilizer.loc[6040])
+print(irrigation.loc[6040])
+print(tillage.loc[6040])
+print(soyb_yield.loc[6040])
+
+#store the relevant columns of dependent and explaining variables in a dictonary
+data = {"lat": soyb_yield.loc[:,'lats'],
+		"lon": soyb_yield.loc[:,'lons'],
+		"area": soyb_yield.loc[:,'growArea'],
+        "yield": soyb_yield.loc[:,'yieldPerArea'],
+		"n_fertilizer": fertilizer.loc[:,'n'],
+		"p_fertilizer": fertilizer.loc[:,'p'],
+        "pesticides_L": pesticides.loc[:,'total_L'],
+        "pesticides_H": pesticides.loc[:,'total_H'],
+        "mechanized": tillage.loc[:,'maiz_mech'],
+        "non-mechanized": tillage.loc[:,'maiz_non_mech'],
+        "irrigation": irrigation.loc[:,'area']
+		}
+
+#compile the data from the dictonary in a pandas dataframe
+dsoyb = pd.DataFrame(data=data)
+#select all rows from dsoyb for which the column area has a value greater than zero
+dsoyb_nozero=dsoyb.loc[dsoyb['area'] > 0]
 
 '''
 #subplot for all histograms
