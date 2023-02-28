62b7b8637b1a8f3cc9b24b22d95978e4bfdee1ee
Morgan Rivers <danielmorganrivers@gmail.com>
2023-02-27 20:15:51 -0800

    fixed import commands

--- src/utilities/stat_ut.py
+++ src/utilities/stat_ut.py
@@ -1,55 +1,57 @@
-'''
-
+"""
 Useful functions involving statistical operations, called from various locations in the code.
-
-'''
+"""
 import os
 import sys
-module_path = os.path.abspath(os.path.join('../..'))
+
+module_path = os.path.abspath(os.path.join("../.."))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from src.utilities import params  # get file location and varname parameters 
+from src.utilities import params  # get file location and varname parameters
 
 import numpy as np
 import pandas as pd
 from operator import itemgetter
-#import statsmodels.api as sm
+
+# import statsmodels.api as sm
 
 params.importIfNotAlready()
 
 
-#I could also update this with the df.apply lambda method (see weighted_average function and application in preprocessing)
+# I could also update this with the df.apply lambda method (see weighted_average function and application in preprocessing)
 def weighted_mean_zonal(df, levels, weights):
     groups = np.sort(levels.unique()).astype(np.int64)
-    df_l = pd.concat([levels, df], axis='columns')
-    col = list(range(1,len(df_l.columns)))
+    df_l = pd.concat([levels, df], axis="columns")
+    col = list(range(1, len(df_l.columns)))
     lists = [[] for g in range(0, len(groups))]
-    for g in groups:        
-        df_g = df_l.loc[df_l.iloc[:,0]==g]
+    for g in groups:
+        df_g = df_l.loc[df_l.iloc[:, 0] == g]
         w_g = weights[df_g.index]
         for c in col:
-            w_a = round(np.average(df_g.iloc[:,c], weights=w_g), 2)
-            #here I need to append the list with the result
-            lists[g-1].append(w_a)
-    results = pd.DataFrame(lists, index = [groups], columns=[df.columns])
+            w_a = round(np.average(df_g.iloc[:, c], weights=w_g), 2)
+            # here I need to append the list with the result
+            lists[g - 1].append(w_a)
+    results = pd.DataFrame(lists, index=[groups], columns=[df.columns])
     return results
 
-#updated version for series, applicable to dfs via df.apply(lambda x:)
+
+# updated version for series, applicable to dfs via df.apply(lambda x:)
 def weighted_average(data, weights, *, dropna: bool = False):
     df = pd.concat([data, weights], axis=1)
     if dropna == True:
         df = df.dropna()
-    weight_mean = round(np.average(df.iloc[:,0], weights=df.iloc[:,1]), 2)
+    weight_mean = round(np.average(df.iloc[:, 0], weights=df.iloc[:, 1]), 2)
     return weight_mean
+
 
 def weighted_mode(data, weights, *, dropna: bool = False):
     if dropna == True:
         data = data.dropna()
     categories = np.sort(data.unique())
     counts = dict.fromkeys(categories)
-    for cat in categories:        
-        data_cat = data.loc[data==cat]
+    for cat in categories:
+        data_cat = data.loc[data == cat]
         if data_cat.empty:
             counts[cat] = 0
         else:
@@ -58,4 +60,3 @@ def weighted_mode(data, weights, *, dropna: bool = False):
             counts[cat] = weighted_count
     weight_mode = max(counts.items(), key=itemgetter(1))[0]
     return weight_mode
-
